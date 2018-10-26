053527953a09b47b7d72550298fa0bbc84a19947
j.butajlo <j.butajlo@samsung.com>
2018-10-26 17:01:11 +0200

    Fragment DSL, Profile Info Tab: data binding, count posts by user id use case, viewmodel

---
+++ app/src/main/java/com/butajlo/koinandroidapp/screen/profile/TabAdapter.kt
@@ -0,0 +1,2 @@
+package com.butajlo.koinandroidapp.screen.profile
+
--- app/src/main/java/com/butajlo/koinandroidapp/ui/FragmentExtensions.kt
+++ app/src/main/java/com/butajlo/koinandroidapp/ui/FragmentExtensions.kt
@@ -1,5 +1,9 @@
 package com.butajlo.koinandroidapp.ui
 
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
 inline fun <reified T : Any> Fragment.argument(key: String) = lazy { arguments?.get(key) as T }
+
+inline fun <reified T : ViewDataBinding> Fragment.binding() = lazy { view?.let { DataBindingUtil.bind<T>(it) } }
