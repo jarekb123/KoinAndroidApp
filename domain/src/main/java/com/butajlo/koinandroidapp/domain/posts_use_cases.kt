ec77ea274f51200af0172f80c15cdde7114f27b2
j.butajlo <j.butajlo@samsung.com>
2018-10-29 13:39:49 +0100

    List extension - random(), random Post By user use case

--- domain/src/main/java/com/butajlo/koinandroidapp/domain/posts_use_cases.kt
+++ domain/src/main/java/com/butajlo/koinandroidapp/domain/posts_use_cases.kt
@@ -1,6 +1,9 @@
 package com.butajlo.koinandroidapp.domain
 
+import com.butajlo.koinandroidapp.domain.entity.PostEntity
+import com.butajlo.koinandroidapp.domain.ext.random
 import com.butajlo.koinandroidapp.domain.repository.PlaceholderRepository
+import io.reactivex.Single
 
 /**
  * Count Articles By User Use Case
@@ -9,7 +12,19 @@ import com.butajlo.koinandroidapp.domain.repository.PlaceholderRepository
  * @param userId User's id
  * @return Number of posts written by user with provided [userId]
  */
-fun countPostsByUser(repository: PlaceholderRepository, userId: Long) =
+fun countPostsByUser(repository: PlaceholderRepository, userId: Long): Single<Int> =
     repository
         .findPostsByUserId(userId)
         .map { it.size }
+
+/**
+ * Random Post By User Use Case
+ *
+ * @param repository Provides data
+ * @param userId User's id
+ * @return Random post written by user with provided [userId]
+ */
+fun randomPostByUser(repository: PlaceholderRepository, userId: Long): Single<PostEntity> =
+        repository
+            .findPostsByUserId(userId)
+            .map { it.random() }
